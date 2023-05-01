0497560a1289ed82d0aabe2a3fa779fe32d7be95
mallabiju <mallabiju@gmail.com>
2023-05-01 20:45:56 +0545

    first commit

---
+++ 1_Basics/c_class_object_method/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace c_class_object_method;
+class Student
+{
+    public void display()
+    {
+        Console.WriteLine("Hello Sanothimi");
+    }
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.display();
+    }
+}
---
+++ 1_Basics/d_class_object_data_members/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace d_class_object_data_members;
+#nullable disable
+class Student
+{
+    int roll;
+    String Name;
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.roll = 23;
+        s1.Name = "Ram Thapa";
+        Console.WriteLine("Roll = " + s1.roll);
+        Console.WriteLine("Name = " + s1.Name);
+    }
+}
