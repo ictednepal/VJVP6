4f8b945f8df1b35eb06d8da834b09130ad6b8686
mallabiju <mallabiju@gmail.com>
2023-05-02 06:48:30 +0545

    Control Statement

---
+++ 2_Control_Statement/b_if_else_postive_negative/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace b_if_else_postive_negative;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number : ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num > 0)
+        {
+            Console.WriteLine("The number " + num + " is a positive number.");
+        }
+        else
+        {
+            Console.WriteLine("The number " + num + " is a zero or negative number.");
+        }
+    }
+}
---
+++ 2_Control_Statement/c_if_elseif_ladder_division/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace c_if_elseif_ladder_division;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter marks in Nepali :");
+        int nepali = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter marks in English :");
+        int english = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter marks in Math :");
+        int math = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter marks in Science :");
+        int science = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter marks in Social :");
+        int social = Convert.ToInt32(Console.ReadLine());
+
+        int total = nepali + english + math + science + social;
+
+        int percentage = total / 5;
+
+        Console.WriteLine("Total marks : " + total);
+        Console.WriteLine("Percentage : " + percentage);
+
+        if (percentage >= 80)
+        {
+            Console.WriteLine("Distinction");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("First");
+        }
+        else if (percentage >= 45)
+        {
+            Console.WriteLine("Second");
+        }
+        else if (percentage >= 32)
+        {
+            Console.WriteLine("Third");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
+    }
+}
