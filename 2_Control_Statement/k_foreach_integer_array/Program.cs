888122b7a078e42b03baaf3a72e793eda5f1d6c7
mallabiju <mallabiju@gmail.com>
2023-05-07 06:29:20 +0545

    New updates

---
+++ 2_Control_Statement/k_foreach_integer_array/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace k_foreach_integer_array;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] marks = { 34, 56, 78, 90, 55, 67 };
+        foreach (var item in marks)
+        {
+            Console.Write(item + " ");
+        }
+
+    }
+}
