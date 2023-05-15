b7854e8de15e8f68bad27b4b41706ec5543a237f
mallabiju <mallabiju@gmail.com>
2023-05-15 07:45:05 +0545

    OOP Class n Object and Structure

---
+++ 4_Class_Object/c_class_object/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace c_class_object;
+#nullable disable
+class Student
+{
+    public int roll;
+    public string name;
+
+    public void input( int r, string n)
+    {
+        roll = r;
+        name = n;
+    }
+    public void output()
+    {
+        Console.WriteLine("Roll no: "+roll);
+        Console.WriteLine("Name: "+name);
+    }
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        s1.input(12,"Ram Rai");
+        s2.input(13,"Sita Thapa");
+        s1.output();
+        s2.output();
+    }
+}
---
+++ 5_Structure/a_structure/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace a_structure;
+struct Student
+{
+    public int roll;
+
+    public void getRoll(int roll)
+    {
+        Console.WriteLine("The roll number is = " + roll);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1;
+        s1.roll = 101;
+        s1.getRoll(s1.roll);
+    }
+}
