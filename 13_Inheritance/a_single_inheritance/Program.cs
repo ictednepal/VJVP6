a0778b04d304ecc0a5ae268ad2de9ec1e340275f
mallabiju <mallabiju@gmail.com>
2023-05-23 06:45:11 +0545

    New updates

---
+++ 13_Inheritance/a_single_inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace a_single_inheritance;
+
+
+class SuperClass{
+    public void hi(){
+        Console.WriteLine("Hi, this is super class");
+    }
+}
+class SubClass : SuperClass{
+    public void hello(){
+        Console.WriteLine("Hello, this is sub class");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SubClass obj = new SubClass();
+        obj.hi();
+        obj.hello();
+    }
+}
