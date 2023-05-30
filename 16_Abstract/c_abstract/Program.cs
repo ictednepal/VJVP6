02aff3075fdc3193c54ab7b4def223fd3f9596ed
mallabiju <mallabiju@gmail.com>
2023-05-30 07:41:56 +0545

    New updates

---
+++ 16_Abstract/c_abstract/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace c_abstract;
+
+public abstract class MyCalc
+{
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+}
+public class ClassTwo : MyCalc
+{
+    public int mul(int a, int b)
+    {
+        return a * b;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClassTwo c1 = new ClassTwo();
+        int result = c1.add(10, 20);
+        Console.WriteLine("The sum = " + result);
+    }
+}
---
+++ 16_Abstract/d_abstract/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace d_abstract;
+#nullable disable
+
+public abstract class NetflixPlan
+{
+    protected double rate;
+    public abstract void getRate();
+    public void calculate(int units)
+    {
+        Console.WriteLine("The amount for {0} units is = {1}", units, units * rate);
+    }
+}
+public class GoldUserPlan : NetflixPlan
+{
+    public override void getRate()
+    {
+        rate = 2.5;
+    }
+}
+public class DiamondUserPlan : NetflixPlan
+{
+    public override void getRate()
+    {
+        rate = 5.0;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        NetflixPlan user;
+        Console.WriteLine("Gold User Plan");
+        user = new GoldUserPlan();
+        user.getRate();
+        user.calculate(245);
+        Console.WriteLine("----------------");
+        Console.WriteLine("Diamond User Plan");
+        user = new DiamondUserPlan();
+        user.getRate();
+        user.calculate(455);
+    }
+}
