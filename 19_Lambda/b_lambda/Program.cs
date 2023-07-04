c11e695ca7b9c18a6b85be4f92fc739fc3df57aa
mallabiju <mallabiju@gmail.com>
2023-07-04 07:16:12 +0545

    Lambda Expression

---
+++ 19_Lambda/b_lambda/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace b_lambda;
+delegate int myDel(int x, int y);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        myDel d = (a, b) => a * b;
+        int result = d(23, 45);
+        Console.WriteLine("The result =" + result);
+    }
+}
---
+++ 19_Lambda/c_lambda/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace c_lambda;
+
+delegate void myDel(int x, int y);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        myDel d = (a, b) =>
+        {
+            Console.WriteLine("The result = " + (a * b));
+            Console.WriteLine("End of the program");
+        };
+        d(23, 45);
+    }
+}
