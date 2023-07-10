214afbd695a369745d0fc81543a6f36701ec4198
mallabiju <mallabiju@gmail.com>
2023-07-10 07:00:50 +0545

    Events

---
+++ 20_Events/a_events/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace a_events;
+public delegate string MyDel(string str);
+
+class EventPro
+{
+    event MyDel MyEvent;
+
+    public EventPro()
+    {
+        this.MyEvent += new MyDel(this.WelcomeUser);
+    }
+    public string WelcomeUser(string username)
+    {
+        return "Welcome " + username;
+    }
+    static void Main(string[] args)
+    {
+        EventPro obj1 = new EventPro();
+        string result = obj1.MyEvent("Hello Sanothimi");
+        Console.WriteLine(result);
+    }
+}
---
+++ 20_Events/b_events/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace b_events;
+#nullable disable
+delegate void OrderEventHandler();
+class Order
+{
+    // declaring an event
+    public event OrderEventHandler OnCreated;
+
+    public void Create()
+    {
+        Console.WriteLine("Order created");
+
+        // raising an event
+        if (OnCreated != null)
+        {
+            OnCreated();
+        }
+    }
+}
+class Email
+{
+    public static void Send()
+    {
+        // @ is known as a verbatim identifier. If a string contains @ as a prefix followed by double quotes, then compiler identifies that string as a verbatim string and compile that string. The main advantage of @ symbol is to tell the string constructor to ignore escape characters and line breaks.
+        Console.WriteLine($"Send an email");
+    }
+}
+class SMS
+{
+    public static void Send()
+    {
+        Console.WriteLine($"Send an SMS");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // subscribing to an event
+        var order = new Order();
+        order.OnCreated += Email.Send;
+        order.OnCreated += SMS.Send;
+        order.Create();
+    }
+}
