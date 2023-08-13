c8897a2fc13a9765c2e015433c21709588f5026b
mallabiju <mallabiju@gmail.com>
2023-08-13 07:36:03 +0545

    LINQ

---
+++ 21_LINQ/b_linq/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace b_linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] Students = {"Ram", "Sita", "Hari", "Gita", "Nita", "Mita"};
+        var nameSort = from name in Students orderby name select name;
+        foreach (var name in nameSort)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
